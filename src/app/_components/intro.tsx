9e4cc7c00408ad0ba3202a7131e30b0ace58ed3d
jiminseong <iamjms4237@gachon.ac.kr>
2025-02-08 18:18:14 +0900

    css:불필요css제거2

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -2,7 +2,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h3 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
-        지민성의 블로그
+        Jiminseong.
       </h3>
     </section>
   );
